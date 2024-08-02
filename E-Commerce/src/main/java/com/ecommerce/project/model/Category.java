5ff766f0c182c77dc032777f56d36dbea6df9923
VY <veasna.youphon@live.com>
2024-08-02 15:25:07 -0700

    Add GlobalExceptionHandler for RestController

--- E-Commerce/src/main/java/com/ecommerce/project/model/Category.java
+++ E-Commerce/src/main/java/com/ecommerce/project/model/Category.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,8 +22,9 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
 
-    //Using validator to check field is not blank
+    //Using validator to check field is not blank and size
     @NotBlank
+    @Size(min = 5, message = "Category name must contain at least 5 characters")
     private String categoryName;
 
 
