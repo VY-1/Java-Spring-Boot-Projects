19ace91545aa8ca12e346eafbfa20593807fb58b
VY <veasna.youphon@live.com>
2024-08-02 21:11:52 -0700

    Add Custom APIException class and implement functionality

--- E-Commerce/src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ E-Commerce/src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -1,5 +1,6 @@
 package com.ecommerce.project.service;
 
+import com.ecommerce.project.exceptions.APIException;
 import com.ecommerce.project.exceptions.ResourceNotFoundException;
 import com.ecommerce.project.model.Category;
 import com.ecommerce.project.repositories.CategoryRepository;
@@ -30,7 +31,12 @@ public class CategoryServiceImpl implements CategoryService{
 
     @Override
     public void createCategory(Category category) {
-        category.setCategoryId(idCounter++);
+        Category savedCategory = categoryRepository.findByCategoryName(category.getCategoryName());
+
+        if(savedCategory != null) {
+            throw new APIException("Category with the name " + category.getCategoryName() + " already exists!");
+        }
+        //category.setCategoryId(idCounter++);
         categoryRepository.save(category);
     }
 
