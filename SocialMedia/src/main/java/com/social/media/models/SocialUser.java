c1de9348c3f0a1cfe9d94e18969ebecea1a7b37a
VY <veasna.youphon@live.com>
2024-08-03 22:42:01 -0700

    Add SocialMedia Example project

---
+++ SocialMedia/src/main/java/com/social/media/models/SocialUser.java
@@ -0,0 +1,21 @@
+package com.social.media.models;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class SocialUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    //@JoinColumn(name = "social_profile_id")
+    private SocialProfile socialProfile;
+
+    @OneToMany(mappedBy = "socialUser")
+    private List<Post> posts = new ArrayList<>();
+
+}
