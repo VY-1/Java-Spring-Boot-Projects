94c66ee952aaa15c4191e9ea41bc67df4d8e9b66
VY <veasna.youphon@live.com>
2024-08-05 15:57:23 -0700

    Add InitializeData class to project

--- SocialMedia/src/main/java/com/social/media/SocialMediaApplication.java
+++ SocialMedia/src/main/java/com/social/media/SocialMediaApplication.java
@@ -1,5 +1,6 @@
 package com.social.media;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +10,7 @@ public class SocialMediaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SocialMediaApplication.class, args);
 	}
+
+
 
 }
--- SocialMedia/src/main/java/com/social/media/models/SocialProfile.java
+++ SocialMedia/src/main/java/com/social/media/models/SocialProfile.java
@@ -1,7 +1,15 @@
 package com.social.media.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class SocialProfile {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -10,5 +18,8 @@ public class SocialProfile {
     //add a relationship
     @OneToOne
     @JoinColumn(name = "social_user")
+    @JsonIgnore
     private SocialUser user;
+
+    private String description;
 }
